9f7008dcb3c0c2be53bd9a54adba892fb44888cd
hudhafah13 <hellohudhafah@gmail.com>
2026-08-22 22:33:31 +0200

    Fix Gradle settings repository mode

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
 pluginManagement {
     repositories {
         google()
@@ -8,7 +10,6 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-
     repositories {
         google()
         mavenCentral()
